03fe83d51d8bb5797bf6a6238fd96955df7b1542
DinaSharipova <sharipova0308@gmail.com>
2024-06-04 17:51:02 +0500

    add main

---
+++ js/script.js
@@ -0,0 +1,6 @@
+//about tabs
+$('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+    $(this)
+      .addClass('active').siblings().removeClass('active')
+      .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+  });
