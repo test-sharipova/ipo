76287d8d9020e564565a36bec78a5aba72133703
DinaSharipova <sharipova0308@gmail.com>
2024-06-05 16:33:40 +0500

    add phone

--- js/script.js
+++ js/script.js
@@ -55,3 +55,13 @@ $('.teachers__slider').slick({
   slidesToScroll: 1,
 
 });
+
+//маска для телефона
+
+let element = document.querySelectorAll('.phone');
+let maskOptions = {
+    mask: '000-000-00-00'
+};
+for (let i = 0; i < element.length; i++) {
+    let mask = IMask(element[i], maskOptions);
+}
