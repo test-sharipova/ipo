ed34f1df90ed69a72c1f9fbc84e454baef155e85
DinaSharipova <sharipova0308@gmail.com>
2024-06-05 12:13:06 +0500

    add tabs

--- js/script.js
+++ js/script.js
@@ -4,3 +4,39 @@ $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
       .addClass('active').siblings().removeClass('active')
       .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
   });
+
+//Программа курса больше 
+          
+var visibleBlocks = 4;
+var allBlocks = $('.about__programm__details').length;
+        
+$('.about__programm__details').slice(visibleBlocks).hide();
+        
+$('.about__programm__more').on('click', function() {
+          visibleBlocks += 4;
+          
+  if (visibleBlocks >= allBlocks) {
+    $('.about__programm__more').hide();
+  }
+           
+  $('.about__programm__details').slice(0, visibleBlocks).show();
+});
+
+//slider
+$('.about__licens__slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+  dots: false,
+  fade: true,
+  asNavFor: '.about__licens__slider-nav'
+});
+$('.about__licens__slider-nav').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.about__licens__slider-for',
+  dots: false,
+  centerMode: true,
+  focusOnSelect: true,
+  
+});
